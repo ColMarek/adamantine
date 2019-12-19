62107f9115e96a292891699497cc0402fc68db26
ColMarek <colmarek254@gmail.com>
2019-12-19 10:33:28 +0300

    Initial commit

---
+++ src/main/kotlin/com/colmarek/adamantine/AdamantineMod.kt
@@ -0,0 +1,13 @@
+package com.colmarek.adamantine
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("unused")
+class AdamantineMod : ModInitializer {
+    companion object {
+        const val MODID = "adamantine"
+    }
+
+    override fun onInitialize() {
+    }
+}
