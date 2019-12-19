dbbac0fe0e6f01ed0f33c1aea2fdd2fa3a0e044d
ColMarek <colmarek254@gmail.com>
2019-12-19 10:35:13 +0300

    Add item group

--- src/main/kotlin/com/colmarek/adamantine/AdamantineMod.kt
+++ src/main/kotlin/com/colmarek/adamantine/AdamantineMod.kt
@@ -1,11 +1,15 @@
 package com.colmarek.adamantine
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
 
 @Suppress("unused")
 class AdamantineMod : ModInitializer {
     companion object {
         const val MODID = "adamantine"
+        val ITEM_GROUP: ItemGroup = FabricItemGroupBuilder.create(Identifier(MODID, "general" )).build()
     }
 
     override fun onInitialize() {
